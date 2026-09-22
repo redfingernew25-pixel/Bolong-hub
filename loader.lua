27d76c3eb3c9181f26f02c009768e2574dd9c023
redfingernew25-pixel <redfingernew25@gmail.com>
2026-09-23 00:19:59 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/redfingernew25-pixel/Bolong-hub/main/loader.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/redfingernew25-pixel/Bolong-hub/main/hub.lua"))()
