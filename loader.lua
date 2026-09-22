4250845f5fd344d60f3c3a8ce1484f0c67dd80f1
redfingernew25-pixel <redfingernew25@gmail.com>
2026-09-23 00:17:08 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,1 @@
--- BOLONG HUB LOADER
--- Repo: redfingernew25-pixel/Bolong-hub
-loadstring(game:HttpGet("https://raw.githubusercontent.com/redfingernew25-pixel/Bolong-hub/main/main/BOLONGHUB.lua"))()
+https://raw.githubusercontent.com/redfingernew25-pixel/Bolong-hub/main/loader.lua
